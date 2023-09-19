638cb4546f7d7247c6d48cc1848ba6dcf976815c
bobby <bobbypoddar2018@gmail.com>
2023-09-19 23:41:51 +0530

    login_page

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ app.get("/home/user",(req,res)=>{
     res.render("user.ejs");
 });
 
+app.get("/home/user/signup",(req,res)=>{
+    res.render("signup.ejs",{port});
+});
+
+app.get("/home/administration/signup",(req,res)=>{
+    res.render("signup.ejs",{port});
+});
+
 app.listen(port,()=>{
     console.log("port is listenig");
 });
